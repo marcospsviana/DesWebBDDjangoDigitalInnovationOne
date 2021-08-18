05f24da10a7fce2adc37e07fd740f5014ad95330
marcospsviana <marcospaulo.silvaviana@gmail.com>
2021-08-18 20:21:47 -0300

    instalado black, formatado arquivos com black

--- core/admin.py
+++ core/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from .models import Events, User
 
+
 @admin.register(Events)
 class EventsAdmin(admin.ModelAdmin):
     model = Events
-    fields = ['title', 'description', 'event_day']\
+    fields = ["title", "description", "event_day"]
 
 
 @admin.register(User)
 class UsersAdmin(admin.ModelAdmin):
     model = User
-    fields = ['email', 'first_name', 'is_staff', 'date_joined', 'password']
+    fields = ["email", "first_name", "is_staff", "date_joined", "password"]
